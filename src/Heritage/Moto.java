68742759993138c689595bbe9bac4943b6b0ecc1
Mkharbech salah eddine <salaheddine.mk8@gmail.com>
2021-11-20 12:19:12 +0100

    exo_vehicule

--- src/Heritage/Moto.java
+++ src/Heritage/Moto.java
@@ -4,7 +4,7 @@ public class Moto extends Vehicule {
 	int nbrRoue = 2;
 		public Moto(String typeCarb, int contenanceReservoir, int contenuReservoir) {
 		super(typeCarb, contenanceReservoir, contenuReservoir);
-		System.out.println("le vehicule à "+nbrRoue +" roues est "+ typeCarb +" sa vitesse maximale est de "+
+		System.out.println("le vehicule est une moto à "+nbrRoue +" roues est "+ typeCarb +" sa vitesse maximale est de "+
 		vitesseMax(280) +" km/h, sa capacite de reservoire est de "+contenanceReservoir+ " et contient "+contenuReservoir+" littres");
 	}
 }
--- src/Heritage/Voiture.java
+++ src/Heritage/Voiture.java
@@ -4,7 +4,7 @@ public class Voiture extends Vehicule {
 	int nbrRoue = 4;
 		public Voiture(String typeCarb, int contenanceReservoir, int contenuReservoir) {
 		super(typeCarb, contenanceReservoir, contenuReservoir);
-		System.out.println("le vehicule à "+nbrRoue +" roues est "+ typeCarb +" sa vitesse maximale est de "+
+		System.out.println("le vehicule est une voiture à "+nbrRoue +" roues est "+ typeCarb +" sa vitesse maximale est de "+
 		vitesseMax(280) +" km/h, sa capacite de reservoire est de "+contenanceReservoir+ " et contient "+contenuReservoir+" littres");
 	}
 }
